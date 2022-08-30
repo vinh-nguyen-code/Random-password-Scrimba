9cf66482db3e3dd384b8b8c8a74bb0b7763e6a81
Vinh1312 <tuanvinhnguyen1312@gmail.com>
2022-08-30 16:50:42 +0200

    Update 1

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+let passwordLength = 8
+let password1 = document.getElementById("password-1")
+let password2 = document.getElementById("password-2")
+let password3 = document.getElementById("password-3")
+let password4 = document.getElementById("password-4")
 
-let suggestionBtn1 = document.getElementById("suggestion-btn-1")
-let suggestionBtn2 = document.getElementById("suggestion-btn-2")
-
-function generate(){
-    for (i=0; i< characters.length;i++){       
-        if(suggestionBtn1.textContent.length<15){
-            suggestionBtn1.textContent += characters[Math.floor(Math.random()*characters.length)]
-        }
-        if(suggestionBtn2.textContent.length<15){
-            suggestionBtn2.textContent += characters[Math.floor(Math.random()*characters.length)]
-        }
+function generatePassword(){
+    let password = ""
+    for (i=0; i< passwordLength;i++){       
+       let randomIndex = Math.floor(Math.random()*characters.length)
+        password += characters[randomIndex]
      }
+     return password
 }
 
+function getPassword(){
+    password1.textContent = generatePassword()
+    password2.textContent = generatePassword()
+    password3.textContent = generatePassword()
+    password4.textContent = generatePassword()
+}
 
+slider.addEventListener("input", function () {
+    sliderNumber.textContent = `${slider.value}`;
+    passwordLength = slider.value;
+  });
